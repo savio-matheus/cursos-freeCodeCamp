bf9e8fa832091983be1636b6c3ab008163a92a3f
Sávio Matheus <savio933@gmail.com>
2022-07-07 19:05:29 -0300

    feat: carrossel com menu de acesso

--- jads/js/script.js
+++ jads/js/script.js
@@ -11,22 +11,42 @@ class Carousel {
         this.slidesContainer = carousel.querySelector("#carousel-slides");
 
         this.currentSlide = 0;
+        this.previousSlide = 0;
         this.numSlides = this.slidesContainer.children.length;
 
         this.buttonPrev.addEventListener("click", this.onPrevious.bind(this));
         this.buttonNext.addEventListener("click", this.onNext.bind(this));
+
+        this.updateScreen();
     }
 
     onPrevious() {
         this.currentSlide = this.integerModulo(this.currentSlide - 1, this.numSlides);
-        this.carousel.style.setProperty("--current-slide", this.currentSlide);
+        this.updateScreen();
         //console.log("onPrevious");
     }
 
     onNext() {
         this.currentSlide = this.integerModulo(this.currentSlide + 1, this.numSlides);
-        this.carousel.style.setProperty("--current-slide", this.currentSlide);
+        this.updateScreen();
         //console.log("onNext");
+    }
+
+    setCurrentSlide(slideNumber) {
+        this.currentSlide = slideNumber;
+        this.updateScreen();
+    }
+
+    updateScreen() {
+        this.carousel.style.setProperty("--current-slide", this.currentSlide);
+
+        let menuButton = document.querySelector(`.navbar-item:nth-child(${this.previousSlide+1})`);
+        menuButton.style.setProperty("box-shadow", "");
+        
+        menuButton = document.querySelector(`.navbar-item:nth-child(${this.currentSlide+1})`);
+        menuButton.style.setProperty("box-shadow", "inset 0 -3px 0px var(--color-main)");
+
+        this.previousSlide = this.currentSlide;
     }
 
     integerModulo(number, mod) {
@@ -38,5 +58,5 @@ class Carousel {
     }
 }
 
-const carousel = document.getElementById("carousel");
-new Carousel(carousel);
+const carouselElement = document.getElementById("carousel");
+let carousel = new Carousel(carouselElement);
