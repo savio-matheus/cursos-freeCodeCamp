4220bc672b718a85bfd2d00e0fa2fd29357eff8e
Sávio Matheus <savio933@gmail.com>
2022-07-09 11:07:23 -0300

    add: desafio do palíndromo

--- jads/js/final1-palindrome-checker.js
+++ jads/js/final1-palindrome-checker.js
@@ -2,7 +2,7 @@ function palindrome(str) {
 	str = str.toLowerCase();
 	let arr = str.match(/[A-Za-z0-9]/gi);
 
-	console.log(arr);
+	//console.log(arr);
 
 	let arrLen = arr.length;
 	let firstHalf = arr.slice(0, Math.floor(arrLen / 2));
@@ -20,5 +20,24 @@ function palindrome(str) {
 	}
 	return true;
 }
+
+let output0 = document.getElementById("output-0");
+let input0 = document.getElementById("input-0");
+
+input0.addEventListener("input", function (e) {
+	let text = "";
+
+	if (input0.value) {
+		if (palindrome(input0.value)) {
+			text = "É palíndromo";
+		} else {
+			text = "Não é palíndromo";
+		}
+	} else {
+		text = "...";
+	}
+
+	output0.innerText = text;
+});
 
 //console.log(palindrome("race car"));
